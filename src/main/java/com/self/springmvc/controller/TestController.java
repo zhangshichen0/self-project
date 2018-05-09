59a136c495f075c82b35ce0da34d216341023fc6
shichen <shichen@17shihui.com>
2018-05-09 12:02:01 +0800

    修改包路径

--- src/main/java/com/self/springmvc/controller/TestController.java
+++ src/main/java/com/self/springmvc/controller/TestController.java
@@ -1,4 +1,4 @@
-package com.self.springmvc;
+package com.self.springmvc.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
