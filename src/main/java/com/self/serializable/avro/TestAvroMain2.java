c81b993435ac5a4717126525773b75273036abdd
shichen <shichen@17shihui.com>
2018-05-11 17:50:09 +0800

    增加avro和json互转

--- src/main/java/com/self/serializable/avro/TestAvroMain2.java
+++ src/main/java/com/self/serializable/avro/TestAvroMain2.java
@@ -2,19 +2,18 @@ package com.self.serializable.avro;
 
 import org.apache.avro.Schema;
 import org.apache.avro.file.DataFileReader;
+import org.apache.avro.file.DataFileStream;
 import org.apache.avro.file.DataFileWriter;
 import org.apache.avro.file.SeekableByteArrayInput;
 import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericDatumReader;
+import org.apache.avro.generic.GenericDatumWriter;
 import org.apache.avro.generic.GenericRecord;
-import org.apache.avro.io.DatumReader;
-import org.apache.avro.io.DatumWriter;
+import org.apache.avro.io.*;
 import org.apache.avro.specific.SpecificDatumReader;
 import org.apache.avro.specific.SpecificDatumWriter;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -37,6 +36,8 @@ public class TestAvroMain2 {
 
         List<GenericRecord> list = new ArrayList<>();
         list.add(article);
+
+        System.out.println(avroToJson(jsonToAvro("{\"id\": 1, \"title\": \"test\"}", schema)));
 
         //序列化
         serializeAvroToFile(list, schema, "avro2.avro");
@@ -125,6 +126,67 @@ public class TestAvroMain2 {
         while (dataFileReader.hasNext()) {
             readUser = dataFileReader.next(readUser);
             System.out.println(readUser);
+        }
+    }
+
+    public static byte[] jsonToAvro(String json, Schema schema) throws IOException {
+        InputStream input = null;
+        DataFileWriter<GenericRecord> writer = null;
+        Encoder encoder = null;
+        ByteArrayOutputStream output = null;
+        try {
+            DatumReader<GenericRecord> reader = new GenericDatumReader<>(schema);
+            input = new ByteArrayInputStream(json.getBytes());
+            output = new ByteArrayOutputStream();
+            DataInputStream din = new DataInputStream(input);
+            writer = new DataFileWriter<>(new GenericDatumWriter<>());
+            writer.create(schema, output);
+            Decoder decoder = DecoderFactory.get().jsonDecoder(schema, din);
+            GenericRecord datum;
+            while (true) {
+                try {
+                    datum = reader.read(null, decoder);
+                } catch (EOFException eofe) {
+                    break;
+                }
+                writer.append(datum);
+            }
+            writer.flush();
+            return output.toByteArray();
+        } finally {
+            try {
+                input.close();
+            } catch (Exception e) {
+            }
+        }
+    }
+
+    public static String avroToJson(byte[] avro) throws IOException {
+        boolean pretty = false;
+        GenericDatumReader<GenericRecord> reader = null;
+        JsonEncoder encoder = null;
+        ByteArrayOutputStream output = null;
+        try {
+            reader = new GenericDatumReader<>();
+            InputStream input = new ByteArrayInputStream(avro);
+            DataFileStream<GenericRecord> streamReader = new DataFileStream<>(input, reader);
+            output = new ByteArrayOutputStream();
+            Schema schema = streamReader.getSchema();
+            DatumWriter<GenericRecord> writer = new GenericDatumWriter<>(schema);
+            encoder = EncoderFactory.get().jsonEncoder(schema, output, pretty);
+            for (GenericRecord datum : streamReader) {
+                writer.write(datum, encoder);
+            }
+            encoder.flush();
+            output.flush();
+            return new String(output.toByteArray());
+        } finally {
+            try {
+                if (output != null) {
+                    output.close();
+                }
+            } catch (Exception e) {
+            }
         }
     }
 
