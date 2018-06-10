ff1b2ad2b3e773bd0a0aeb890d85c6640e42b00d
shichen <shichen@17shihui.com>
2018-06-10 16:39:29 +0800

    验证synchronized的wait逻辑 从功能上来说wait就是说线程在获取对象锁后，主动释放对象锁，同时本线程休眠，加入到等待线程队列中

---
+++ src/main/java/com/self/lock/TestWait.java
@@ -0,0 +1,55 @@
+package com.self.lock;
+
+/**
+ * 验证synchronized的wait逻辑
+ *
+ * @author shichen
+ * @create 2018/6/10
+ * @desc
+ */
+public class TestWait {
+
+    private Object lock = new Object();
+
+    public void testWait() {
+        long before = System.currentTimeMillis();
+        synchronized (lock) {
+            System.out.println("获得锁，并等待" + Thread.currentThread().getId());
+            try {
+                //从功能上来说wait就是说线程在获取对象锁后，主动释放对象锁，同时本线程休眠，加入到等待线程队列中
+                lock.wait(10000);
+                //sleep并不释放对象锁
+                Thread.sleep(10000);
+                System.out.println("等待1s");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
+        System.out.println("执行完成" + Thread.currentThread().getId());
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        TestWait testWait = new TestWait();
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                testWait.testWait();
+            }
+        });
+
+        Thread.sleep(1000);
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                testWait.testWait();
+            }
+        });
+
+        thread.start();
+        thread1.start();
+
+    }
+
+}
