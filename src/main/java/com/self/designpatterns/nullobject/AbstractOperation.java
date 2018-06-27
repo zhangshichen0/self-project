a8a1efd19b22e409088fc349ad3fd1955b26b640
shichen <shichen@17shihui.com>
2018-06-27 16:00:16 +0800

    空对象模式

---
+++ src/main/java/com/self/designpatterns/nullobject/AbstractOperation.java
@@ -0,0 +1,15 @@
+package com.self.designpatterns.nullobject;
+
+/**
+ * @author shichen
+ * @create 2018/6/27
+ * @desc
+ */
+public abstract class AbstractOperation {
+
+    /**
+     * 操作
+     */
+    public abstract void request();
+
+}
---
+++ src/main/java/com/self/designpatterns/nullobject/NullOperation.java
@@ -0,0 +1,9 @@
+package com.self.designpatterns.nullobject;
+
+/**
+ * @author shichen
+ * @create 2018/6/27
+ * @desc
+ */
+public class NullOperation {
+}
