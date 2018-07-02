e9463fd19f7170e2c20b034e747e3a940a68b587
shichen <shichen@17shihui.com>
2018-07-02 11:54:29 +0800

    享元模式

---
+++ src/main/java/com/self/designpatterns/flyweight/Flyweight.java
@@ -0,0 +1,15 @@
+package com.self.designpatterns.flyweight;
+
+/**
+ * @author shichen
+ * @create 2018/7/2
+ * @desc
+ */
+public interface Flyweight {
+    /**
+     * 操作
+     *
+     * @param extrinsicState
+     */
+    void doOperation(String extrinsicState);
+}
