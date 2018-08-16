d8b4fb38579048b211a5f04f463f208981bc80de
shichen <shichen@17shihui.com>
2018-08-16 15:41:07 +0800

    增加方法上使用Synchronized关键字

--- src/main/java/com/self/lock/sync_lock/ObjectLockMainTest.java
+++ src/main/java/com/self/lock/sync_lock/ObjectLockMainTest.java
@@ -30,6 +30,15 @@ public class ObjectLockMainTest {
         }
     }
 
+    public synchronized void lock3() {
+        System.out.println("当前线程：" + Thread.currentThread().getName() + "获得锁");
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
 
         ObjectLockMainTest objectLockMainTest = new ObjectLockMainTest();
@@ -48,8 +57,16 @@ public class ObjectLockMainTest {
             }
         }, "2");
 
+        Thread thread3 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                objectLockMainTest.lock3();
+            }
+        }, "3");
+
         thread1.start();
         thread2.start();
+        thread3.start();
     }
 
 }
