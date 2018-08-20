e94689503ec4be2fd57db6095e7e808ab0b96ebc
shichen <shichen@17shihui.com>
2018-08-20 10:27:43 +0800

    增加锁说明

--- src/main/java/com/self/lock/reentrant_lock/ReentrantLockTest.java
+++ src/main/java/com/self/lock/reentrant_lock/ReentrantLockTest.java
@@ -9,6 +9,15 @@ import java.util.concurrent.locks.ReentrantLock;
  */
 public class ReentrantLockTest {
 
+    /**
+     * 步骤【非公平锁为例】：
+     * 1.线程1尝试获取锁，由于现在锁空闲，线程1获取成功，处理业务
+     * 2.此时线程2尝试获取锁，由于锁被线程1占着，所以获取不成功
+     * 3.线程2加入到锁链表中【双向链表】，自旋锁修改线程2所在node中的waitStatus=Signal，调用LockSupport.park挂起线程2，等待被唤醒
+     * 4.线程1业务处理完毕，释放锁，同时调用LockSupport.unpark唤醒锁链表中head的下一个节点中的线程2
+     * 5.线程2获取锁，循环往复1--4
+     */
+
     private static ReentrantLock lock = new ReentrantLock();
 
     public static void main(String[] args) throws InterruptedException {
