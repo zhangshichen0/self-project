cdb599e31efb7d74bb49e8c33b996b9a3718aab8
shichen <shichen@17shihui.com>
2018-09-10 09:30:25 +0800

    netty server初始化流程 registered bind active

--- src/main/java/com/self/netty/server/NettyServerHandler.java
+++ src/main/java/com/self/netty/server/NettyServerHandler.java
@@ -2,6 +2,9 @@ package com.self.netty.server;
 
 import io.netty.channel.ChannelDuplexHandler;
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelPromise;
+
+import java.net.SocketAddress;
 
 /**
  * @author shichen
@@ -13,16 +16,19 @@ public class NettyServerHandler extends ChannelDuplexHandler {
     @Override
     public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
         System.out.println("netty server register");
-
         super.channelRegistered(ctx);
+    }
 
 
+    @Override
+    public void bind(ChannelHandlerContext ctx, SocketAddress localAddress, ChannelPromise promise) throws Exception {
+        System.out.println("netty server bind");
+        super.bind(ctx, localAddress, promise);
     }
 
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
         System.out.println("netty server active");
-
         super.channelActive(ctx);
     }
 }
