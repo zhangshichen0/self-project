5bdcc3ab4417c45d0f807e33262ea52b50a6dd74
zhangshichen <shichen@17shihui.com>
2019-08-01 15:24:52 +0800

    验证字符串常量池被放在堆区

--- src/main/java/com/self/jvm/StringPoolMainTest.java
+++ src/main/java/com/self/jvm/StringPoolMainTest.java
@@ -1,4 +1,4 @@
-package com.self.jdk8;
+package com.self.jvm;
 
 /**
  * 字符串常量池在jdk8中默认存在于堆中
@@ -10,8 +10,9 @@ package com.self.jdk8;
 public class StringPoolMainTest {
 
     public static void main(String[] args) {
+        int i = 0;
         while (true) {
-            String a = new String("我查的大幅度大幅度发大发送到发而发的发生大嘎达发").intern();
+            String a = new String("我查的大幅度大幅度发大发送到发而发的发生大嘎达发" + (i ++)).intern();
         }
     }
 
