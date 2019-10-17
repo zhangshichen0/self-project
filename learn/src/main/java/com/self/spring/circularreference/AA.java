502502021815b6313eb162f5da9b0c5d9b7f48e6
zhangshichen <shichen@17shihui.com>
2019-10-17 16:32:29 +0800

    增加循环依赖测试

---
+++ learn/src/main/java/com/self/spring/circularreference/AA.java
@@ -0,0 +1,12 @@
+package com.self.spring.circularreference;
+
+/**
+ * @author shichen
+ * @create 2019-10-17
+ * @desc
+ */
+public interface AA {
+
+    public void a();
+
+}
---
+++ learn/src/main/java/com/self/spring/circularreference/BB.java
@@ -0,0 +1,12 @@
+package com.self.spring.circularreference;
+
+/**
+ * @author shichen
+ * @create 2019-10-17
+ * @desc
+ */
+public interface BB {
+
+    public void b();
+
+}
